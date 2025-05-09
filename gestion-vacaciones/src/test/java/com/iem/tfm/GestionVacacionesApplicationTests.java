cfe4099e0a2e0b32916f966d6d2b8a1615efb9d4
InigoSanz <i.sanzdelg@gmail.com>
2025-05-09 18:17:30 +0200

    Update

---
+++ gestion-vacaciones/src/test/java/com/iem/tfm/GestionVacacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iem.tfm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionVacacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
