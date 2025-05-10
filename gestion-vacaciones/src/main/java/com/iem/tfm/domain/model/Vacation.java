3b54e1348d4955c0c473b4754c4a41a5d4844ad5
InigoSanz <i.sanzdelg@gmail.com>
2025-05-10 18:29:00 +0200

    Clase domain model Vacation

--- gestion-vacaciones/src/main/java/com/iem/tfm/domain/model/Vacation.java
+++ gestion-vacaciones/src/main/java/com/iem/tfm/domain/model/Vacation.java
@@ -1,5 +1,43 @@
 package com.iem.tfm.domain.model;
 
-public class Vacation {
+import java.util.Date;
 
+/**
+ * Modelo de dominio para las vacaciones.
+ * 
+ * Encapsula las fechas de inicio y fin de las vacaciones y al
+ * empleado al que pertenecen.
+ * 
+ * @author Inigo
+ * @version 1.0
+ */
+public class Vacation {
+	
+	private Long id;
+	private Date startDate;
+	private Date endDate;
+	private Employee employee;
+	
+	public Vacation(Long id, Date startDate, Date endDate, Employee employee) {
+		this.id = id;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.employee = employee;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public Employee getEmployee() {
+		return employee;
+	}
 }
