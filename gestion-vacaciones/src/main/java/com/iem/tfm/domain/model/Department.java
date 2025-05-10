c9f4bb512df4b39f5c11dcc1b6296c0d923c0ed4
InigoSanz <i.sanzdelg@gmail.com>
2025-05-10 18:24:10 +0200

    Clase domain model Department

--- gestion-vacaciones/src/main/java/com/iem/tfm/domain/model/Department.java
+++ gestion-vacaciones/src/main/java/com/iem/tfm/domain/model/Department.java
@@ -1,5 +1,30 @@
 package com.iem.tfm.domain.model;
 
+/**
+ * Modelo de dominio para los departamentos.
+ * 
+ * Contiene la información básica de un departamento. 
+ * 
+ * Se utiliza para asociar empleados a uno o más departamentos.
+ * 
+ * @author Inigo
+ * @version 1.0
+ */
 public class Department {
+	
+	private Long id;
+	private String name;
+	
+	public Department(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
 
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
 }
