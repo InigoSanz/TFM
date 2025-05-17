3232656a2b743f8e4e471baaf068dc01af6bff4e
InigoSanz <i.sanzdelg@gmail.com>
2025-05-17 11:52:36 +0200

    Endpoint para obtener todos los departamentos

--- gestion-vacaciones/src/main/java/com/iem/tfm/infrastructure/apirest/controller/DepartmentController.java
+++ gestion-vacaciones/src/main/java/com/iem/tfm/infrastructure/apirest/controller/DepartmentController.java
@@ -1,7 +1,16 @@
 package com.iem.tfm.infrastructure.apirest.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.iem.tfm.application.port.output.DepartmentRepositoryOutputPort;
+import com.iem.tfm.infrastructure.apirest.dto.response.DepartmentResponseDto;
+import com.iem.tfm.infrastructure.database.mapper.DepartmentDtoMapper;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,8 +24,23 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/department")
 @Slf4j
 public class DepartmentController {
-	
-	// TODO: Obtener todos los departamentos
-	
+
+	@Autowired
+	DepartmentRepositoryOutputPort departmentRepositoryOutput;
+
+	@Autowired
+	DepartmentDtoMapper departmentDtoMapper;
+
+	@GetMapping
+	public ResponseEntity<List<DepartmentResponseDto>> getDepartments() {
+		log.debug("-> Petición para obtener todos los departamentos recibida <-");
+
+		List<DepartmentResponseDto> responseDtoList = departmentDtoMapper.toDtoList(departmentRepositoryOutput.findAll());
+
+		log.debug("-> Departamentos obtenidos exitosamente <-");
+		
+		return ResponseEntity.ok(responseDtoList);
+	}
+
 	// TODO: Consultar un departamento por ID
 }
