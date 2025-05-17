4084a93161dbe8d5c0ecbbacdba7545b9a090234
InigoSanz <i.sanzdelg@gmail.com>
2025-05-17 11:45:40 +0200

    Empezamos con el controlador para los departamentos

---
+++ gestion-vacaciones/src/main/java/com/iem/tfm/infrastructure/apirest/controller/DepartmentController.java
@@ -0,0 +1,22 @@
+package com.iem.tfm.infrastructure.apirest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Controlador REST para los endpoints de los empleados.
+ * 
+ * @author Inigo
+ * @version 1.0
+ */
+@RestController
+@RequestMapping("/department")
+@Slf4j
+public class DepartmentController {
+	
+	// TODO: Obtener todos los departamentos
+	
+	// TODO: Consultar un departamento por ID
+}
