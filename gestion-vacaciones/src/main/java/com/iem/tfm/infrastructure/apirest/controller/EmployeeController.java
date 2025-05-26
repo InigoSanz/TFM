cf5d4706bc3301cfb21656e3aa35b10ae76bd192
InigoSanz <i.sanzdelg@gmail.com>
2025-05-26 19:17:39 +0200

    Habilitamos los CORS para la conexión con el frontend

--- gestion-vacaciones/src/main/java/com/iem/tfm/infrastructure/apirest/controller/EmployeeController.java
+++ gestion-vacaciones/src/main/java/com/iem/tfm/infrastructure/apirest/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,6 +36,7 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping("/employee")
 @Slf4j
+@CrossOrigin(origins = "http://localhost:4200")
 public class EmployeeController {
 	
 	@Autowired
