9082216f9004cc8b1b9a62a5b6ae142543889337
InigoSanz <i.sanzdelg@gmail.com>
2025-06-14 19:34:05 +0200

    Creamos el DTO para modificar los datos de un empleado

---
+++ gestion-vacaciones/src/main/java/com/iem/tfm/infrastructure/apirest/dto/request/EmployeeUpdateRequestDto.java
@@ -0,0 +1,28 @@
+package com.iem.tfm.infrastructure.apirest.dto.request;
+
+import java.util.Date;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.jackson.Jacksonized;
+
+/**
+ * 
+ */
+@Data
+@Builder
+@Jacksonized
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeUpdateRequestDto {
+	
+	private int age;
+	private String email;
+	private Date startDate;
+	private Date endDate;
+	private List<String> departmentIds;
+	private String role;
+}
